c2d7d1da59583bb17a7c0eea6cd0e0eac58ac1fd
wongok <tkddjs0116@gmail.com>
2022-05-21 20:49:39 +0900

    Initial commit

---
+++ src/main/java/sping/boot/springbatch/SpringbootSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package sping.boot.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringbatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sping/boot/springbatch/SpringbootSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package sping.boot.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringbatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
