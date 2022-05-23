236ea540d3196130e6c1b216c04df9ad28688130
wongok <tkddjs0116@gmail.com>
2022-05-23 20:19:10 +0900

    Spring Batch - BATCH_JOB_EXECUTION / 실패 및 성공 처리

--- src/main/java/sping/boot/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/sping/boot/springbatch/job/SimpleJobConfiguration.java
@@ -25,6 +25,7 @@ public class SimpleJobConfiguration {
         // job의 이름은 빌더를 통해 지정
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -37,7 +38,21 @@ public class SimpleJobConfiguration {
         // 1개의 Step = Tasklet 1개 + Reader & Processor & Writer
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
+                    // 1. 에러 발생 (예외 처리) -> 2. 정상 실행 : 같은 JOB_INSTANCE_ID로 JOB_EXECUTION_ID가 발번 됨, 성공한 JOB_INSTANCE_ID가 있으면 에러 발생
+                    // throw new IllegalArgumentException("step1에서 실패합니다.");
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestData = {}", requestData);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestData]}") String requestData) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
                     log.info(">>>>> requestData = {}", requestData);
                     return RepeatStatus.FINISHED;
                 })
